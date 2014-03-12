c462349d69c62f20b0b0b1f9801e9412cac37c34
jsripatana <jsripatana@yahoo.com>
2014-03-12 20:03:04 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JoeApp::Application.config.secret_key_base = 'ed38b5e088d8f4d2ab50a88c1faf4575302619d4ae03ff1a968c3845d09bd196beff72203c298a756b534fe9ac71e158d06820b9f6e72e5085c4ae984fc91e88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JoeApp::Application.config.session_store :cookie_store, key: '_joe_app_session'
